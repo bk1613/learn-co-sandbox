19d6bee0dca4c54322c0ef81f63802ed95d042e0
Brian Kao <bk1613@nyu.edu>
2019-09-16 03:01:54 +0000

    Automatically backed up by Learn

---
+++ learning-commits.rb
@@ -0,0 +1,21 @@
+# Don't do authenticate_or_request_with_http_digest
+puts ( 1 )#+ 1)
+
+temperature = 70
+if temperature <= 50
+  puts "Bundle up!"
+elsif (temperature > 50 && temperature < 70)
+  puts "Hoodie weather!"
+else
+  puts "It's a scorcher!"
+end
+
+nums = [1,2,3,4]
+
+
+
+i = 0
+
+while nums[i] do
+  nums.collect{|n| n * n}
+end
